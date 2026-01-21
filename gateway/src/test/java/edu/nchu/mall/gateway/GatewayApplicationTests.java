9e3224ff6653cb8ae0b6af146a8c0ef3d1199a62
barry2llen <2768566033@qq.com>
2026-01-21 16:00:21 +0800

    添加网关gateway

---
+++ gateway/src/test/java/edu/nchu/mall/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nchu.mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
